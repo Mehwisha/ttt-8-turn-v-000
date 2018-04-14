c83e4fcc50234e425137686ae222360f990c7493
Mehwish Aziz <mehwisha@msn.com>
2018-04-14 01:30:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,6 +23,8 @@ def position_taken?(board, index)
   board[index] != " "
 end
 def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
   input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player = "X")
